73bb7055664239574ff0d4cda600195647a8a6f2
NYPD <nypd54@outlook.com>
2016-12-13 16:20:30 -0500

    slowly adding tests to the frameowrk

--- test/core-test.js
+++ test/core-test.js
@@ -1,13 +1,18 @@
 /* eslint-env mocha */
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+
+var jsdom = require('jsdom').jsdom;
+global.document = jsdom('<html><body></body></html>');
+global.window = document.defaultView;
 global.$ = require('jquery');
-var assert = require('chai').assert;
-var expect = require('chai').expect;
-var should = require('chai').should();
+
 var ritsu = require('../dist/ritsu.js');
 
 describe('ritsu', function() {
 
-  describe('#initialize()', function() {
+  describe('initialize();', function() {
 
     it('should throw Error when the options passed in is not an object', function() {
       expect(ritsu.initialize.bind('init')).to.throw(Error);
@@ -26,9 +31,157 @@ describe('ritsu', function() {
 
     });
 
+    after(function() {
+      ritsu.initialize({});
+    });
+
   });
 
-  describe('#storeInitialFormValues()', function() {
+  describe('storeInitialFormValues();', function() {
+
+  });
+
+
+  describe('validate();', function() {
+
+
+    beforeEach(function () {
+        $('body').html('');
+        ritsu.initialize({});
+    });
+
+    it('should validate a input element passed in', function() {
+
+      var input = document.createElement('input');
+      input.type = 'text';
+      input.className = 'alpha alpha-only';
+
+      input.value = 'beans';
+      var $input = $(input);
+      var isValid = ritsu.validate($input);
+      assert.isTrue(isValid);
+
+      input.value = 'bea3ns';
+      $input = $(input);
+      isValid = ritsu.validate($input);
+      assert.isFalse(isValid);
+
+    });
+
+    it('should validate a input element in the document since nothing was passed in', function() {
+
+      var input = document.createElement('input');
+      input.type = 'text';
+      input.className = 'alpha alpha-only';
+
+      input.value = 'beans';
+      $('body').html(input);
+      var isValid = ritsu.validate();
+      assert.isTrue(isValid);
+
+      input.value = 'bea3ns';
+      $('body').html(input);
+      isValid = ritsu.validate();
+      assert.isFalse(isValid);
+
+    });
+
+    it('should validate a input element based on the jQuery container passed in', function() {
+
+      $('body').append('<div class="cool"></div>');
+
+      var input = document.createElement('input');
+      input.type = 'text';
+      input.className = 'alpha alpha-only';
+
+      input.value = 'beans';
+     $('.cool').html(input);
+      var isValid = ritsu.validate($('.cool'));
+      assert.isTrue(isValid);
+
+      input.value = 'bea3ns';
+      $('.cool').html(input);
+      isValid = ritsu.validate($('.cool'));
+      assert.isFalse(isValid);
+
+    });
+
+    it('should validate a select element passed in', function() {
+
+      var select = document.createElement('select');
+      var option = document.createElement('option');
+
+      option.text = 'cool option';
+      select.appendChild(option);
+
+      var $select = $(select);
+      var isValid = ritsu.validate($select);
+      assert.isTrue(isValid);
+
+      option.text = '';
+
+      select.options[0] = null;
+      select.appendChild(option);
+      $select = $(select);
+
+      isValid = ritsu.validate($select);
+      assert.isFalse(isValid);
+
+    });
+
+    it('should mark a input element passed in with a data attribute of invalid = true', function() {
+
+      var input = document.createElement('input');
+      input.type = 'text';
+      input.className = 'alpha alpha-only';
+
+      input.value = 'bean3s';
+      var $input = $(input);
+      ritsu.validate($input);
+
+      var invalid = $input.data('invalid') === true;
+      assert.isTrue(invalid);
+
+    });
+
+    it('should add a has-error class to the input element passed in', function() {
+
+      var input = document.createElement('input');
+      input.type = 'text';
+      input.className = 'alpha alpha-only';
+      input.value = 'bean3s';
+
+      var $input = $(input);
+
+      ritsu.validate($input);
+
+      var hasError = $input.hasClass('has-error');
+      assert.isTrue(hasError);
+
+    });
+
+    it('should add a has-error class to the form-group of the input element passed in', function() {
+
+      ritsu.initialize({
+        useBootstrap3Stlying: true
+      });
+
+      $('body').append('<div class="form-group"></div>');
+
+      var input = document.createElement('input');
+      input.type = 'text';
+      input.className = 'alpha alpha-only';
+      input.value = 'bean3s';
+
+      var $formGroup = $('.form-group');
+      $formGroup.append(input);
+
+      ritsu.validate();
+
+      var hasError = $formGroup.hasClass('has-error');
+      assert.isTrue(hasError);
+
+    });
 
   });
 
--- test/rules-test.js
+++ test/rules-test.js
@@ -1,15 +1,21 @@
 /* eslint-env mocha */
 var chai = require('chai');
 var assert = chai.assert;
+var expect = chai.expect;
 var rules = require('../src/rules.js');
+
+var jsdom = require('jsdom').jsdom;
+var window = jsdom().defaultView;
+var document = window.document;
+
 
 describe('rules', function() {
 
-  describe('#getRuleByRuleClass()', function() {
+  describe('getRuleByRuleClass();', function() {
 
     it('should return the correct rule by class name', function() {
       var rule = rules.getRuleByRuleClass('alpha-only');
-      assert.isOk(rule);
+      assert.isNotNull(rule);
 
       assert.equal(rule.ruleType, 'alpha');
       assert.equal(rule.getErrorMessage(), 'Only letters, spaces, hypens, and periods are allowed');
@@ -23,24 +29,149 @@ describe('rules', function() {
   });
 
 
-  describe('#addValidationRule()', function() {
+  describe('addValidationRule();', function() {
 
-    it('should add a new validation rule', function() {
+    it('should add a new validation rule with multiple parameters', function() {
 
       var rule = rules.getRuleByRuleClass('numeric-beans');
-      assert.isNotOk(rule);
+      assert.isNull(rule);
 
       var ruleType = 'numeric';
       var ruleClass = 'numeric-beans';
-      var validationFunction = function () {};
+      var validationFunction = function() {};
 
       rules.addValidationRule(ruleType, ruleClass, validationFunction);
 
       rule = rules.getRuleByRuleClass('numeric-beans');
-      assert.isOk(rule);
+      assert.isNotNull(rule);
     });
 
-    it('should add multiple validation rule');
+    it('should add a new validation rule with a rule object', function() {
+
+      var rule = rules.getRuleByRuleClass('numeric-fruit');
+      assert.isNull(rule);
+
+      var validationFunction = function() {};
+
+      rules.addValidationRule({
+        ruleType: 'numeric',
+        ruleClass: 'numeric-fruit',
+        validationFunction: validationFunction
+      });
+
+      rule = rules.getRuleByRuleClass('numeric-fruit');
+      assert.isNotNull(rule);
+
+    });
+
+    it('should add multiple validation rules', function() {
+
+      var validationFunction = function() {};
+
+      var ruleSilly = rules.getRuleByRuleClass('numeric-silly');
+      var ruleSilly2 = rules.getRuleByRuleClass('numeric-silly2');
+
+      assert.isNull(ruleSilly);
+      assert.isNull(ruleSilly2);
+
+      rules.addValidationRule([{
+        ruleType: 'numeric',
+        ruleClass: 'numeric-silly',
+        validationFunction: validationFunction
+      }, {
+        ruleType: 'numeric',
+        ruleClass: 'numeric-silly2',
+        validationFunction: validationFunction
+      }]);
+
+      ruleSilly = rules.getRuleByRuleClass('numeric-silly');
+      ruleSilly2 = rules.getRuleByRuleClass('numeric-silly2');
+
+      assert.isNotNull(ruleSilly);
+      assert.isNotNull(ruleSilly2);
+
+    });
+
+    it('should throw an Error when trying to add a rule with an incorrect rule type', function() {
+
+      expect(function() {
+        rules.addValidationRule('string');
+      }).to.throw(Error);
+
+    });
+
+    it('should throw an Error when trying to add a non string rule class', function() {
+
+      expect(function() {
+        rules.addValidationRule('alpha', {});
+      }).to.throw(Error);
+
+    });
+
+    it('should throw an Error when trying to add a non function validation function', function() {
+
+      expect(function() {
+        rules.addValidationRule('alpha', 'alpha-cool', 54);
+      }).to.throw(Error);
+
+    });
+
+  });
+
+
+  describe('default rules validation;', function() {
+
+    it('alpha-only', function() {
+
+      var rule = rules.getRuleByRuleClass('alpha-only');
+
+      var input = document.createElement('input');
+      input.type = 'text';
+      input.value = 'beans';
+
+      var isValid = rule.validate(input);
+
+      assert.isTrue(isValid);
+
+      input.value = 'b34ns';
+      isValid = rule.validate(input);
+
+      assert.isFalse(isValid);
+
+    });
+
+    it('alpha-zip', function() {
+
+      var rule = rules.getRuleByRuleClass('alpha-zip');
+
+      var input = document.createElement('input');
+      input.type = 'text';
+
+      input.value = '19608';
+      var isValid = rule.validate(input);
+      assert.isTrue(isValid);
+
+      input.value = '19608 8911';
+      isValid = rule.validate(input);
+      assert.isTrue(isValid);
+
+      input.value = '19608-8911';
+      isValid = rule.validate(input);
+      assert.isTrue(isValid);
+
+      input.value = 'A9C1A1';
+      isValid = rule.validate(input);
+      assert.isTrue(isValid);
+
+      input.value = 'A9C 1A1';
+      isValid = rule.validate(input);
+      assert.isTrue(isValid);
+
+      input.value = '19608 12';
+      isValid = rule.validate(input);
+      assert.isFalse(isValid);
+
+    });
 
   });
 
