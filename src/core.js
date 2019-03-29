b9a1452f08f0f8c81ff8d33b75262720feb0d450
Erik Munoz <6412879+NYPD@users.noreply.github.com>
2019-03-29 10:39:39 -0400

    V1.4.0 (#112)
    * Deprecate getErrorMessagesAsMap and add getErrorMessagedAsObjects
    
    Closes #109
    
    * Bump version
    
    * Add deprecation notice for getErrorMessagesAsMap()
    
    * Fix wording on test case
    
    * Build v1.4.0

--- src/core.js
+++ src/core.js
@@ -240,7 +240,15 @@ var core = function core(rules, validation) {
 
   };
 
+  /**
+   * @deprecated since v1.4.0
+   */
   var getErrorMessagesAsMap = function getErrorMessagesAsMap(selector) {
+
+    // eslint-disable-next-line no-console
+    console.warn(
+      'getErrorMessagesAsMap has been deprecated since v1.4.0. Use getErrorMessagesAsObjects instead.'
+    );
 
     var elementArray = _getSelectorAsElementArray(selector);
 
@@ -252,6 +260,28 @@ var core = function core(rules, validation) {
     });
 
     return errorMessageMap;
+
+  };
+
+  var getErrorMessagesAsObjects = function getErrorMessagesAsMap(selector) {
+
+    var elementArray = _getSelectorAsElementArray(selector);
+
+    var invalidErrorObjects = elementArray.reduce(function(accumulator, element) {
+
+      var isInvalid = element.getAttribute('data-invalid') === 'true';
+      if (isInvalid) {
+        accumulator.push({
+          'input': element,
+          'errorMessage': _getErrorMessageForInput(element)
+        });
+      }
+
+      return accumulator;
+
+    }, []);
+
+    return invalidErrorObjects;
 
   };
 
@@ -456,7 +486,8 @@ var core = function core(rules, validation) {
     showErrorMessages: showErrorMessages,
     getErrorMessage: getErrorMessage,
     getErrorMessages: getErrorMessages,
-    getErrorMessagesAsMap: getErrorMessagesAsMap
+    getErrorMessagesAsMap: getErrorMessagesAsMap,
+    getErrorMessagesAsObjects: getErrorMessagesAsObjects
 
   };
 
